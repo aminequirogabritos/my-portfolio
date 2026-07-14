5a3840e7e7df5d99907700f79e3b5417c2b6c28e
amine <aminequirogabritos@gmail.com>
2026-07-14 10:36:35 -0300

    comment out configuration for testing purposes

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,20 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.argv.includes('dev');
+// const dev = process.argv.includes('dev');
 
 export default {
-	preprocess: vitePreprocess(),
+	// preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
 			fallback: '404.html'
-		}),
-		paths: {
+		})
+		/* 		paths: {
 			base: process.argv.includes('dev') ? '' : '/my-portfolio'
 		},
-		outDir: '.svelte-kit/output'
+		outDir: '.svelte-kit/output' */
 	}
 };
