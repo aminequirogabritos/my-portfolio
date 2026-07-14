a1e1aa3d56367074283b1af46a59b811a6dc0994
amine <aminequirogabritos@gmail.com>
2026-07-14 11:46:19 -0300

    fix: enable preprocessing in Svelte configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
-// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-// const dev = process.argv.includes('dev');
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-	// preprocess: vitePreprocess(),
+	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
 			fallback: '404.html'
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : '/my-portfolio'
 		}
-		// outDir: '.svelte-kit/output'
 	}
 };
